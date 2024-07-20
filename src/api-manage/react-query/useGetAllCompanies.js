84bcc454ba2fcd0456be4fb3fab734256fef91a7
Jalaj <jalaj.nagwani@crewman.in>
2024-07-20 12:45:07 +0530

    Retrieved token from localstorage through utils, Used to token in ComapnyList component to fetch the data

--- src/api-manage/react-query/useGetAllCompanies.js
+++ src/api-manage/react-query/useGetAllCompanies.js
@@ -1,10 +1,19 @@
 import MainApi from "../MainApi";
 import { useQuery } from "react-query";
 import { get_companies } from "../ApiRoutes";
+import { getToken } from "@/utils/getToken";
 
 
 const getCompanies = async () => {
-  const { data } = await MainApi.get(get_companies);
+  const token = getToken();
+  const { data } = await MainApi.get(
+    get_companies,
+    {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    }
+  );
   return data;
 };
 
