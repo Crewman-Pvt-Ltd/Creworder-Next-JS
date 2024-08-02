4545e0b9dac0f12ab4ad7bc207267aaac51ba65c
priyacrewman <priya.singh@crewman.in>
2024-08-02 11:23:57 +0530

    employee data dynamic , changes in notice make required fields

--- src/pages/superadmin/Notice/index.js
+++ src/pages/superadmin/Notice/index.js
@@ -1,29 +1,17 @@
 import React from "react";
-import NoticeList from "@/components/Notice/NoticeList";
+import NoticeList from "@/components/notice/NoticeList";
 import Layout from "@/components/Layout";
-import CreateNotice from "@/components/Notice/CreateNotice";
-import { useState } from "react";
+
 import {
  
 } from "@mui/material";
 
 const Index = () => {
-  const [showCreateNotice, setShowCreateNotice] = useState(false);
 
-  const handleShowCreateNotice = () => {
-    setShowCreateNotice(true);
-  };
-  const handleBackToList = () => {
-      setShowCreateNotice(false);
-    };
 
   return (
     <Layout>
-      {showCreateNotice ? (
-        <CreateNotice onNoticeList={handleBackToList} />
-      ) : (
-        <NoticeList onNotice={handleShowCreateNotice} />
-      )}
+     <NoticeList />
     </Layout>
   );
 };
