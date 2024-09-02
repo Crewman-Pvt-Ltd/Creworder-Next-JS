62f9bb96fc9a81d27c85ff5824fbd3f297fcf393
shivam-kumar-sain <shivamkumar6399029@gmail.com>
2024-09-02 14:33:53 +0530

    orderexport or view

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import Navitem from './Navitem';
 import { Collapse, List, ListItem, ListItemText, Box, Typography, useMediaQuery, useTheme } from '@mui/material';
 import { useRouter } from 'next/router';
-import DashboardIcon from '@mui/icons-material/home';
+import DashboardIcon from '@mui/icons-material/Home';
+
 import { Download as DownloadIcon, Height, Margin } from "@mui/icons-material";
 import ReceiptIcon from '@mui/icons-material/ShoppingCart';
 import RocketLaunchIcon from '@mui/icons-material/RocketLaunch';
