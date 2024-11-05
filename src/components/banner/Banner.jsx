b13ce44d5fe903750c01c3db4c9345852c9004f3
priyacrewman <priya.singh@crewman.in>
2024-11-05 13:01:31 +0530

    Create Order

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,38 +1,39 @@
-
 import React from 'react';
 import Slider from 'react-slick';
-import sliderImages1 from '../../images/slider-design.png';
-import sliderImages2 from '../../images/78249.png';
+import sliderImages1 from '../../images/sliderdesign.png';
+import sliderImages2 from '../../images/78249qwqw.png';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import Image from 'next/image';
+
 const Banner = () => {
   const settings = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    dots: false,             
+    infinite: true,          
+    speed: 800,              
+    slidesToShow: 1,         
+    slidesToScroll: 1,       
+    autoplay: true,          
+    autoplaySpeed: 3000,    
   };
 
   return (   
-      <Slider {...settings} style={{ width: '1250px'}}>
+      <Slider {...settings} style={{ width: '1250px' }}>
         <div>
-           <Image 
-              src={sliderImages1}
-              alt="Slider 1"
-              style={{ width: '100%', height: 'auto', display: 'block'}} 
-              />
+          <Image 
+            src={sliderImages1}
+            alt="Slider 1"
+            style={{ width: '100%', height: 'auto', display: 'block'}} 
+          />
         </div>
         <div>
-        <Image 
-              src={sliderImages2}
-              alt="Slider 1"
-              style={{ width: '100%', height: 'auto', display: 'block'}} 
-              />
+          <Image 
+            src={sliderImages2}
+            alt="Slider 2"
+            style={{ width: '100%', height: 'auto', display: 'block'}} 
+          />
         </div>
       </Slider>
- 
   );
 };
 
